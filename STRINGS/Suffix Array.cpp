165bcabc10a531093ef6edab8814fdbab5ceb4b4
Arfatul Islam Asif <107148686+ArfatulAsif@users.noreply.github.com>
2025-11-10 16:38:47 +0600

    Update Suffix Array.cpp

--- STRINGS/Suffix Array.cpp
+++ STRINGS/Suffix Array.cpp
@@ -677,8 +677,8 @@ int32_t main()
 
 ================================== Longest Palindromic substring (LPS) ================
 
-s = s +"#"+rev(s) 
-finding max lcp of this string is the answer.
+// s = s +"#"+rev(s) 
+// finding max lcp of this string is the answer.
 
 	
 
